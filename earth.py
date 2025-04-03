1892c7811ca32a11fd1f6d543a7d5e1f617b9173
jceguaburo <jceguaburo@mail.austral.edu.ar>
2025-04-03 17:32:44 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,10 +1,10 @@
 def earth():
-    country1 = "Bangladesh"
-    country2 = "Barbados"
+    x = "Bangladesh"
+    y = "Barbados"
 
-    result1 = country1 < country2
-    result2 = country2 < country1
+    result1 = x < y
+    result2 = y < x
 
-    print(f"The result of {country1} comes first in the dictionary than {country2} is {result1}.")
-    print(f"The result of {country2} comes first in the dictionary than {country1} is {result2}.")
+    print(f"The result of {x} comes first in the dictionary than {y} is {result1}.")
+    print(f"The result of {y} comes first in the dictionary than {x} is {result2}.")
     
